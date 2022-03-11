98b15b97e3ae4f4e6bc1063ee0b40446e04b59dd
Shoban Chiddarth <messageshoban@gmail.com>
2022-03-11 16:29:13 +0530

    Updated src/main.py
    Added `add patient` functionality

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 import mysql.connector as connector
 import prettytable
 from prettytable import from_db_cursor
+import json
+import requests
+import sys
 
 print('''\
 SRI SAKTHI DENTAL CLINIC
@@ -20,6 +23,55 @@ if connection.is_connected():
 Welcome admin
 Tyoe `help` for help
 ''')
+
+    def randomstring(chars, nums='on', upper='on', lower='off') -> str:
+        '''\
+Random String Generator
+
+Function to use the form at https://www.random.org/strings/
+(modified as per the requirements of the program)
+
+RANDOM.ORG Documentation
+------------------------
+This form allows you to generate random text strings. 
+The randomness comes from atmospheric noise, which for 
+many purposes is better than the pseudo-random number 
+algorithms typically used in computer programs.
+
+PARAMETERS
+----------
+chars : length of the string
+nums : Numeric digits (0-9)
+upper : Uppercase letters (A-Z)
+lower : Lowercase letters (a-z)
+
+OUTPUT
+------
+Return a random string'''
+
+        r=requests.get(f'https://www.random.org/strings/?num=1&len={chars}&digits={nums}&upperalpha={upper}&loweralpha={lower}&unique=on&format=plain&rnd=new')
+        return r.content.decode('utf-8')
+    
+    def table_from_db(table:str):
+        '''Return the given table name as prettytable from database'''
+        cursor=connection.cursor()
+        cursor.execute(f'SELECT * FROM {table};')
+        table = from_db_cursor(cursor)
+        return table
+    
+    def multi_line_input(
+    margin='| ',
+    end='\n',
+    error=KeyboardInterrupt
+):
+        line=input(margin)
+        while True:
+            try:
+                line=line+end+input(margin)
+            except error as e:
+                sys.stdout.write('\r\n')
+                return line
+
     while True:
         command=input('Enter command> ')
         command=command.strip()
@@ -96,15 +148,62 @@ NOTE: You can remove appointments only if the treatment didn\'t take place''',
         elif command.lower() in ('exit', 'quit'):
             break
 
-        elif command.strip()=='show patients':
-            cursor=connection.cursor()
-            cursor.execute('SELECT * FROM patients;')
-            mytable = from_db_cursor(cursor)
+        elif command=='show patients':
+            mytable = table_from_db('patients')
             mytable.align='l'
             print(mytable)
+        
+        elif command=='add patient':
+            print('Adding new patient in table `patients` in database `SrisakthiPatients`')
+
+            patientID=randomstring(7)
+            patientcursor=connection.cursor()
+            patientcursor.execute('SELECT patientID from patients;')
+            iddata=patientcursor.fetchall()
+            patientIDlist=[id for id in iddata]
+            while patientID in patientIDlist:
+
+                patientID=randomstring(7)
+
+            del patientcursor, iddata, patientIDlist
+
+            name=input('Enter patient name: ')
+
+            print('''Date Of Birth Format: YYYY-MM-DD
+Example: 1999-03-12''')
+            dob=input('Enter dob: ')
+            while not (dob[0:4].isdigit() and 
+                        dob[4]=='-' and 
+                        dob[5:7].isdigit() and 
+                        dob[7]=='-' and
+                        dob[8:10].isdigit()
+                        and len(dob)==10 ):
+                    print('Invalid DOB')
+                    dob=input('Re-enter DOB: ')
+
+            phone=input('Enter phone number with country code: ')
+
+            while True:
+                gender=input('Enter gender (M/F) : ')
+                if gender in ('M', "F"):
+                    break
+                else:
+                    print('Invalid gender')
+                    print('Re-enter gender')
+
+            print('Enter address:')
+            address=multi_line_input()
+
+            cursor=connection.cursor()
+            cursor.execute(f'''INSERT INTO patients (patientID, name, dob, gender, phone, address)
+VALUES ("{patientID}", "{name}", '{dob}', "{gender}", "{phone}", "{address}");''')
+            connection.commit()
+
+            print('New patient created')
+
+
     
     print('Exited')
-    input()
     connection.close()
 else:
     print('Connection to MySQL Database SriSakthiPatients FAILED')
