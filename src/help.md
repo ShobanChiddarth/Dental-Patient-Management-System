1e8a161462dddbea6b52994c301188ec8622f6ee
Shoban Chiddarth <messageshoban@gmail.com>
2022-03-11 14:03:26 +0530

    Created the main script (until help utilities)
    help is fully completed
    
    - src/main.py -> the main script
    - src/help.md -> help text

---
+++ src/help.md
@@ -0,0 +1,43 @@
+COMMANDS
+========
+
+GENERAL
+-------
+- help: show this message
+- exit: exit the script
+- quit: alias of exit
+
+PATIENTS
+--------
+- show patients: print the table `patients` in database `SrisakthiPatients`
+- add patient: create a new patient
+               in database `SrisakthiPatients`, table `patients`
+- update patient: update a patient in table `patients` in database `SrisakthiPatients`
+                  You must provide `patientID`
+NOTE: You can\'t delete patients
+
+APPOINTMENTS
+------------
+- show appointments: print the table `Appointments` in database `SrisakthiPatients`
+- add appointment: add a new appointment in table `Appointments` in database `
+- update appointment: update an existing appointment in table `Appointments` in database `SrisakthiPatients`
+                    You must provide `treatmentID`
+- remove appointment: remove an appointment in table `Appointments` in database `SrisakthiPatients`
+                      You must provide `treatmentID`
+NOTE: You can remove appointments only if the treatment didn\'t take place
+
+TREATMENTS
+----------
+- show treatments: print the table `treatments` in database `SrisakthiPatients`
+- add treatment-exact: add new treatment in table `treatments` in database `SrisakthiPatients`
+                       with the exact date and time in table `Appointments`
+                       You must provide `treatmentID`
+- add treatment: add new treatment in table `treatments` in database `SrisakthiPatients`
+                 You must provide `treatmentID`
+- update treatment: update treatment in in table `treatments` in database `SrisakthiPatients`
+                    You must provide `treatmentID`
+NOTE: You can\'t remove treatments
+
+END OF HELP
+
+---
---
+++ src/main.py
@@ -0,0 +1,102 @@
+import mysql.connector as connector
+
+print('''\
+SRI SAKTHI DENTAL CLINIC
+DENTAL PATIENT MANAGEMENT SYSTEM
+''')
+
+connection=connector.connect(
+    host='localhost',
+    port='3306',
+    user='root',
+    password='1234',
+    database='SrisakthiPatients'
+)
+if connection.is_connected():
+    print('Connected to MySQL Database SriSakthiPatients')
+    print('''\
+Welcome admin
+Tyoe `help` for help
+''')
+    while True:
+        command=input('Enter command> ')
+        command=command.strip()
+        if command.lower().startswith('help'):
+            if command=='help':
+                
+                with open('help.md','rt', encoding='utf-8', newline='\r\n') as helpfile:
+                    print(helpfile.read())
+            else:
+                command=command.split()
+                del command[0]
+
+                helpdict={
+                    'help':'show help message',
+                    'exit':'exit the script',
+                    'quit':'alias of exit',
+                    'show':{
+                        'patients':'print the table `patients` in database `SrisakthiPatients`',
+                        'appointments':'print the table `Appointments` in database `SrisakthiPatients`',
+                        'treatments':'print the table `treatments` in database `SrisakthiPatients`',
+                    },
+                    'add':{
+                        'patient':'''\
+create a new patient
+in database `SrisakthiPatients`, table `patients`''',
+                        'appointment':'add a new appointment in table `Appointments` in database `',
+                        'treatment':'''\
+add new treatment in table `treatments` in database `SrisakthiPatients`
+You must provide `treatmentID`''',
+                        'treatment-exact':'''\
+add new treatment in table `treatments` in database `SrisakthiPatients`
+with the exact date and time in table `Appointments`
+You must provide `treatmentID`'''
+                    },
+                    'update':{
+                        'patient':'''\
+update a patient in table `patients` in database `SrisakthiPatients`
+You must provide `patientID`''',
+                        'appointment':'''\
+update an existing appointment in table `Appointments` in database `SrisakthiPatients`
+You must provide `treatmentID`''',
+                        'treatment':'''\
+update treatment in in table `treatments` in database `SrisakthiPatients`
+You must provide `treatmentID`'''
+                    },
+                    'remove':{
+                        'patient':'You can\'t delete patients',
+                        'appointment':'''\
+remove an appointment in table `Appointments` in database `SrisakthiPatients`
+You must provide `treatmentID`
+NOTE: You can remove appointments only if the treatment didn\'t take place''',
+                        'treatment':'You can\'t remove treatments'
+                        }
+                    }
+
+                
+                def helpparse(words, searchdict):
+                    try:
+                        if isinstance(words, list):
+
+                            if isinstance(searchdict[words[0]], str):
+                                return searchdict[words[0]]
+                            elif isinstance(searchdict[words[0]], dict):
+                                searchdict=searchdict[words[0]]
+                                del words[0]
+                                return helpparse(words, searchdict)
+                    except KeyError as k:
+                        return str(k)+'\n'+'Invalid Input'
+                    except IndexError as I:
+                        return 'Invalid Input'
+
+                
+                print(helpparse(command, helpdict))
+        elif command.lower() in ('exit', 'quit'):
+            break
+    
+    print('Exited')
+    connection.close()
+else:
+    print('Connection to MySQL Database SriSakthiPatients FAILED')
+
+
