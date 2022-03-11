e375d30202b9a8ff9ecfc3dc96e6c2feef2ef742
Shoban Chiddarth <messageshoban@gmail.com>
2022-03-11 11:49:11 +0530

    Created MySql Database SriSakthiPatients
    Database SriSakthiPatients
    
    Tables
    - patients
    - Appointments
    - Treatments

---
+++ backup/backup.sql
@@ -0,0 +1,116 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: SrisakthiPatients
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `appointments`
+--
+
+DROP TABLE IF EXISTS `appointments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `appointments` (
+  `Sno` int NOT NULL AUTO_INCREMENT,
+  `date` date DEFAULT NULL,
+  `time` time DEFAULT NULL,
+  `patientID` varchar(10) NOT NULL,
+  `treatmentID` varchar(10) NOT NULL,
+  PRIMARY KEY (`treatmentID`),
+  UNIQUE KEY `Sno` (`Sno`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `appointments`
+--
+
+LOCK TABLES `appointments` WRITE;
+/*!40000 ALTER TABLE `appointments` DISABLE KEYS */;
+INSERT INTO `appointments` VALUES (1,'2022-07-12','10:00:00','ABC1234','A24DS67');
+/*!40000 ALTER TABLE `appointments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `patients`
+--
+
+DROP TABLE IF EXISTS `patients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `patients` (
+  `Sno` int NOT NULL AUTO_INCREMENT,
+  `patientID` varchar(10) NOT NULL,
+  `name` varchar(225) NOT NULL,
+  `dob` date NOT NULL,
+  `gender` char(1) NOT NULL,
+  `phone` varchar(17) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  PRIMARY KEY (`patientID`),
+  UNIQUE KEY `Sno` (`Sno`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `patients`
+--
+
+LOCK TABLES `patients` WRITE;
+/*!40000 ALTER TABLE `patients` DISABLE KEYS */;
+INSERT INTO `patients` VALUES (1,'ABC1234','MUTHU','2000-03-28','M','+919000000000','Cecilia Chapman\\n711-2880 Nulla St.\\nMankato Mississippi 96522');
+/*!40000 ALTER TABLE `patients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `treatments`
+--
+
+DROP TABLE IF EXISTS `treatments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `treatments` (
+  `Sno` int NOT NULL AUTO_INCREMENT,
+  `patientID` varchar(10) NOT NULL,
+  `treatmentID` varchar(10) NOT NULL,
+  `date` date DEFAULT NULL,
+  `time` time DEFAULT NULL,
+  `treatment` text,
+  `status` text,
+  `fee` double(16,2) DEFAULT NULL,
+  `paid` tinyint(1) DEFAULT NULL,
+  UNIQUE KEY `Sno` (`Sno`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `treatments`
+--
+
+LOCK TABLES `treatments` WRITE;
+/*!40000 ALTER TABLE `treatments` DISABLE KEYS */;
+INSERT INTO `treatments` VALUES (1,'ABC1234','A24DS67','2022-07-12','10:07:00','Root Canal','Completed',7000.00,1);
+/*!40000 ALTER TABLE `treatments` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-03-11 11:47:58
---
+++ initial-sql-commands/SriSakthiPatients.sql
@@ -0,0 +1,53 @@
+DROP DATABASE IF EXISTS SrisakthiPatients;
+CREATE DATABASE SrisakthiPatients;
+USE SrisakthiPatients;
+
+CREATE TABLE `patients` (
+  `Sno` int NOT NULL AUTO_INCREMENT,
+  `patientID` varchar(10) NOT NULL,
+  `name` varchar(225) NOT NULL,
+  `dob` date NOT NULL,
+  `gender` char(1) NOT NULL,
+  `phone` varchar(17) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  PRIMARY KEY (`patientID`),
+  UNIQUE KEY `Sno` (`Sno`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO patients (patientID, name, dob, gender, phone, address)
+VALUES ("ABC1234", "MUTHU", '2000-03-28', "M", "+919000000000", "Cecilia Chapman\\n711-2880 Nulla St.\\nMankato Mississippi 96522");
+
+
+
+
+CREATE TABLE `Appointments` (
+  `Sno` int NOT NULL AUTO_INCREMENT,
+  `date` DATE,
+  `time` TIME,
+  `patientID` varchar(10) NOT NULL REFERENCES patients(patientID),
+  `treatmentID` VARCHAR(10),
+  PRIMARY KEY (`treatmentID`),
+  UNIQUE KEY `Sno` (`Sno`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO Appointments (date, time, patientID, treatmentID)
+VALUES ('2022-07-12', '10:00', "ABC1234", "A24DS67");
+
+
+
+
+CREATE TABLE `treatments` (
+    `Sno` int NOT NULL AUTO_INCREMENT,
+    `patientID` varchar(10) NOT NULL REFERENCES patients(patientID),
+    `treatmentID` VARCHAR(10) NOT NULL REFERENCES Appointments(treatmentID),
+    `date` DATE,
+    `time` TIME,
+    `treatment` TEXT,
+    `status` TEXT,
+    `fee` REAL(16,2),
+    `paid` BOOLEAN,
+    UNIQUE KEY `Sno` (`Sno`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO treatments (patientID, treatmentID, date, time, treatment, status, fee, paid)
+VALUES ("ABC1234", "A24DS67", '2022-07-12', '10:07', "Root Canal", "Completed", 7000.00, True);
