daba1360c22f83df4800c20b8c59f0d27d1c484e
Shoban Chiddarth <messageshoban@gmail.com>
2022-05-27 15:20:43 +0530

    DOC: Added and updated docstrings to all functions

--- src/helper/__init__.py
+++ src/helper/__init__.py
@@ -7,9 +7,12 @@ import pprint
 
 
 def notempty(line:str):
+    """Tell if a given line (string) is empty (blank or whitespace) or not"""
     return not (line.isspace() or (not line))             
 
 def mappingTitles(line:str):
+            """\
+If a line is not a title (is not bolded(in markdown) from the begginning), return the line as bulleted list."""
             if line.startswith('**') and (line.count('**')==2):
                 return line
             else:
@@ -19,9 +22,10 @@ def mappingTitles(line:str):
 
 global_flattened_list=[]
 def flattenDict(d : dict,):
-    """Recursively iterate over the given dictionary
+    """\
+Recursively iterate over the given dictionary
 
-And store it in a dictionary `flattenedDict`, later look for the key in it and return the value"""
+And store it in a global list global_flattened_list as a list of tuples of key, value pairs"""
     for k,v in d.items():
         if isinstance(d, dict):
             if isinstance(v, str):
@@ -32,6 +36,13 @@ And store it in a dictionary `flattenedDict`, later look for the key in it and r
             
 
 def helpParse(command : str):
+        """\
+**Parse** the help command
+
+Lstrip('help'), call the function `flattenDict`, iterate over the global list `global_flattened_list`
+(iterate with key,value pair strategy -> like how you would iterate on dict.items())
+and by taking command as the key, return the corresponding value. If key does not exist, return appropriate msg.
+"""
         command=command.lstrip('help').strip()
         flattenDict(_data)
         for key, value in global_flattened_list:
@@ -41,6 +52,10 @@ def helpParse(command : str):
                 return "UNKNOWN COMMAND"
 
 def processHelp(helpCommand : str):
+    """\
+The actual function to be called to **procees** the help statement.
+
+It does a lot of things."""
 
     with open(os.path.join(os.path.dirname(__file__), 'commands.json')) as fh:
         global _data
