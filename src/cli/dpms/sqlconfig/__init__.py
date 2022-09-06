f2637eb54c9b21a22b9423e1155859f42812ea4a
Useless Programmmer <uselessprogrammer@example.com>
2022-09-06 15:08:28 +0530

    DEP: Deleted the cli folder
    as it is no longer needed

--- src/cli/dpms/sqlconfig/__init__.py
+++
@@ -1,7 +0,0 @@
-"""\
-This module handles configuration of SQL connection in the project.
-
-The configuration credentails (data) is stored in the file `sqlcredentials.json`.
-"""
-from . import load
-from . import manage
